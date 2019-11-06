d334a20fe9c3de362ea51843ac0cfb27b4deaa53
distantredmoon <56657684+distantredmoon@users.noreply.github.com>
2019-11-06 14:29:19 -0700

    new enemy

---
+++ EscapeWithGhost/Assets/PlayerHealthDamage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthDamage : MonoBehaviour
+{
+    public int Health = 10;
+    private bool isTouching = true;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        isTouching = true;
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Enemy")
+            Health -= 1;
+    }
+}
