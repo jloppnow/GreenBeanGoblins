5f649beff23336c9b19294f89fe79bbd9488bf6b
nnnathannnnn <33075001+nnnathannnnn@users.noreply.github.com>
2020-03-11 20:40:51 -0600

    A start

---
+++ BakedGoodsBattle/Assets/Scripts/BoardGrid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardGrid : MonoBehaviour
+{
+    [SerializeField]
+    int Rows = 0;
+    [SerializeField]
+    int Columns = 0;
+
+    [SerializeField]
+    Transform BoardTransform = null;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ BakedGoodsBattle/Assets/Scripts/GridNode.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridNode : MonoBehaviour
+{
+    [SerializeField]
+    LayerMask layers;
+
+    [SerializeField]
+    float radius = 0.0f;
+
+    [SerializeField]
+    List<GridNode> connectingNodes = null;
+
+    public void FindNearbyNodes(float radius, Vector3 plane, float radiusLimit = float.MaxValue)
+    {
+        Collider[] nodes = Physics.OverlapBox(transform.position, plane * radius, transform.rotation, layers);
+        foreach (Collider node in nodes)
+        {
+            if (node.gameObject != gameObject)
+            {
+                float dist = Vector3.Distance(transform.position, node.transform.position);
+                if (dist <= radiusLimit)
+                    connectingNodes.Add(node.GetComponent<GridNode>());
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+
+        Gizmos.color = Color.green;
+        foreach(GridNode node in connectingNodes)
+        {
+            Gizmos.DrawLine(transform.position + (Vector3.down * radius), node.transform.position + (Vector3.up * radius));
+        }
+    }
+}
